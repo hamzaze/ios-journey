9776f9327b21b4e38289e7c7fe8e9413cd812c75
Hamza <Hamza@cable-5-43-86-58.dynamic.telemach.ba>
2016-11-02 15:26:58 +0100

    Display selected user details from annotation callout

---
+++ 2210_1_MapKit+Autolayout/2210_1_MapKit+Autolayout/2210_1_MapKit+Autolayout/HHAnnotation.swift
@@ -0,0 +1,21 @@
+//
+//  HHAnnotation.swift
+//  2210_1_MapKit+Autolayout
+//
+//  Created by Hamza on 02/11/2016.
+//  Copyright © 2016 Hamza. All rights reserved.
+//
+
+import UIKit
+
+class HHAnnotation: MKAnnotationView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
