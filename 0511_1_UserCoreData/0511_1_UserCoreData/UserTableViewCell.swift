0b6887d114002cd83b8d29dfa39466a34a3e9d17
Hamza <Hamza@Hamzas-iMac.local>
2016-11-08 01:27:35 +0100

    Core Data with JSON using SwiftyJSON with MAPKit integration, avoiding duplicates using NSPredicate

---
+++ 0511_1_UserCoreData/0511_1_UserCoreData/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  0511_1_UserCoreData
+//
+//  Created by Hamza on 07/11/2016.
+//  Copyright © 2016 Hamza. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
